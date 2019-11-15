994a4df1ed85077265f13f0e7e66ffdb79ac1899
rypatel126 <rypatel126@gmail.com>
2019-11-15 15:11:57 -0500

    Further developed game logic
    user can now enter X or O on a specific board square

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,10 +1,63 @@
-// place X or O when board is clicked
-const marker = 'X OR O'
-const playerOne = 'X'
-const playerTwo = 'O'
 
+// when player clicks a board space, value of that element will be changed
 
+let player = 'O'
 
+// player needs to alternate between X and O
+const switchPlayer = () => {
+  if (player === 'O') {
+    player = 'X'
+  } else {
+    player = 'O'
+  }
+}
+
+const placeMarker = event => {
+  // when player clicks a board space, value of that element will be changed to X
+  console.log('click worked', event)
+  // after x is inputted, switchPlayer will change the value in the board space to O
+  switchPlayer()
+  if ((player === 'X') && ($(event.target).html() === '')) { // if it's X's turn and the box they click is undefined (ie empty), an X or O will be placed in the box depending on turn
+    $(event.target).text(player)
+  } else $(event.target).text(player)
+}
+// ((player === 'O') && ($(event.target).html() === '')) not needed because of else {}
+
+/*
+In order to win the game, player must have 3 of the same markers in a row
+Set board up as an array
+
+board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+where board[i] corresponds to:
+0 1 2
+3 4 5
+6 7 8
+
+Winning combinations:
+0 1 2
+3 4 5
+6 7 8
+0 3 6
+1 4 7
+2 5 8
+0 4 8
+2 4 6
+
+when a player inputs an X or an O, that value is .push-ed into the board array?
+
+// arrays to hold player moves; start as undefined, based on moves they make, value will be inputted into the corresponding playerOne/TwoMoves[i]
+// const playerOneMoves = []
+// const playerTwoMoves = []
+
+iterate through each player array and if any of the winning combinations are present in either array, that player will win.
+*/
+
+const checkForWin = () => [
+
+]
+
+// FAILED CODE IS BELOW:
+//
 // const placeMarker = () => {
 //   console.log('click works')
 //   let marker = playerOne
@@ -17,14 +70,39 @@ const playerTwo = 'O'
 //   }
 // }
 
+// const turns = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
+
+// const playerTurn = turns => {
+//   for (let i = 0; i < turns.length; i++) {
+//     return
+//   }
+// }
+// console.log(playerTurn)
+
 // game starts with player 1
 // const turn = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-// when player 1 clicks a box, it is replaced with an X
+// when player 1 clicks a box, an X appears in the box
 
-const placeMarker = () => {
-  console.log('click worked')
-}
+// const placeMarker = playerMove => {
+//   if ($('.board-space-0').text() === '' && (playerMove % 2) === 1) {
+//     $('.board-space-0').text('X')
+//   } else {
+//     $('.board-space-0').text('O')
+//   }
+//   playerMove++
+// }
 
+// $('.col').on('click', placeMarker)
+//
+// const gameStart = () => {
+//   if (player === 'X') {
+//     xTurn()
+//   } else {
+//     oTurn()
+//   }
+// }
+
+// const xTurn = () => {
 
 // $('.col').text(playerOne)
 
@@ -42,5 +120,7 @@ const placeMarker = () => {
 // }
 
 module.exports = {
-  placeMarker
+  placeMarker,
+  switchPlayer,
+  checkForWin
 }
