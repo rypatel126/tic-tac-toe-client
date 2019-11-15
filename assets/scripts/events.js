1fc0ac307828222f72ffc70bd53298aa347723ae
rypatel126 <rypatel126@gmail.com>
2019-11-15 16:29:35 -0500

    Started to work on win function logic

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -19,7 +19,9 @@ const placeMarker = event => {
   switchPlayer()
   if ((player === 'X') && ($(event.target).html() === '')) { // if it's X's turn and the box they click is undefined (ie empty), an X or O will be placed in the box depending on turn
     $(event.target).text(player)
-  } else $(event.target).text(player)
+  } else {
+    $(event.target).text(player)
+  }
 }
 // ((player === 'O') && ($(event.target).html() === '')) not needed because of else {}
 
@@ -51,10 +53,26 @@ when a player inputs an X or an O, that value is .push-ed into the board array?
 
 iterate through each player array and if any of the winning combinations are present in either array, that player will win.
 */
+const board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
-const checkForWin = () => [
 
-]
+
+const winRow1 = [board[0], board[1], board[2]]
+const winRow2 = [board[3], board[4], board[5]]
+const winRow3 = [board[6], board[7], board[8]]
+const winCol1 = [board[0], board[3], board[6]]
+const winCol2 = [board[1], board[4], board[7]]
+const winCol3 = [board[2], board[5], board[8]]
+const winDiag1 = [board[0], board[4], board[8]]
+const winDiag2 = [board[2], board[4], board[6]]
+
+const gameWin = () => {
+
+}
+
+// const checkForWin = () => {
+//
+// }
 
 // FAILED CODE IS BELOW:
 //
@@ -107,7 +125,7 @@ const checkForWin = () => [
 // $('.col').text(playerOne)
 
 // player 1 is X, player 2 is O
-// X ALWAYS GOES FIRST!
+// X ALWAYS GOES FIRST
 
 // determine which player's turn it is
 // const turn = () => {
@@ -121,6 +139,6 @@ const checkForWin = () => [
 
 module.exports = {
   placeMarker,
-  switchPlayer,
-  checkForWin
+  switchPlayer
+  // checkForWin
 }
