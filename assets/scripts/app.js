161a3628129f252bcfcb255ad08f9f24953bc1c4
rypatel126 <rypatel126@gmail.com>
2019-11-17 15:47:18 -0500

    Started new game format and structure
    Created a new game board and took out the bootstrap styling as it was
    causing styling issues.  Removed all previous failed game logic, new
    logic will be added to take into account the actual board spaces so api
    can save updates more easily

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -8,13 +8,5 @@ const events = require('./events.js')
 // require('./example')
 
 $(() => {
-  $('#bs0').on('click', events.placeMarker)
-  $('#bs1').on('click', events.placeMarker)
-  $('#bs2').on('click', events.placeMarker)
-  $('#bs3').on('click', events.placeMarker)
-  $('#bs4').on('click', events.placeMarker)
-  $('#bs5').on('click', events.placeMarker)
-  $('#bs6').on('click', events.placeMarker)
-  $('#bs7').on('click', events.placeMarker)
-  $('#bs8').on('click', events.placeMarker)
+
 })
